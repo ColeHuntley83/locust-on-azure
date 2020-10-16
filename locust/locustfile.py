03b115f7f1bdc2a47ac9ae94b23c296c82294378
ColeHuntley83 <calrugby1@gmail.com>
2020-10-16 15:24:04 -0700

    format

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -17,7 +17,8 @@ def get_date():
 
 
 def get_alphanum():
-    x = ''.join(random.choice(string.ascii_uppercase  + string.digits) for _ in range(12))
+    x = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                for _ in range(12))
     return x
 
 
@@ -27,7 +28,7 @@ def get_random_num(min, max):
 
 
 def get_random_facility():
-    map = [ 'NJLGN','9T904T','35W35R', 'RE4033','E2565f']
+    map = ['NJLGN', '9T904T', '35W35R', 'RE4033', 'E2565f']
     return map[random.randint(0, len(map)-1)]
 
 
@@ -43,24 +44,26 @@ def mock_data():
             "activdate": get_date(),
             "linkdate": get_date(),
             "devstatus": "1",
-            "ttype" : "White",
+            "ttype": "White",
             "tperson": "Parcel",
             "apn": fake.first_name()
         }
     }
 
+
 def mock_event():
     return {
-    "eventData": {
-        "cid" : get_alphanum(),
-        "ts" : f"{int(time.time())}",
-        "rid" : get_alphanum(),
-        "scantype" : f"{get_random_num(0, 9999)}",
-        "type" : fake.ean13(),
-        "pids" : [f"{get_random_num}", "A4", "F6", "87", "AC", f"{get_random_num(0, 99)}"],
-        "sTimes" : [f"{get_random_num(1000000, 9999999999)}", f"{get_random_num(1000000, 9999999999)}", f"{get_random_num(1000000, 9999999999)}",f"{get_random_num(1000000, 9999999999)}", f"{get_random_num(1000000, 9999999999)}", "1602309674"]
+        "eventData": {
+            "cid": get_alphanum(),
+            "ts": f"{int(time.time())}",
+            "rid": get_alphanum(),
+            "scantype": f"{get_random_num(0, 9999)}",
+            "type": fake.ean13(),
+            "pids": [f"{get_random_num}", "A4", "F6", "87", "AC", f"{get_random_num(0, 99)}"],
+            "sTimes": [f"{get_random_num(1000000, 9999999999)}", f"{get_random_num(1000000, 9999999999)}", f"{get_random_num(1000000, 9999999999)}", f"{get_random_num(1000000, 9999999999)}", f"{get_random_num(1000000, 9999999999)}", "1602309674"]
+        }
     }
-}
+
 
 class APIUser(HttpUser):
     wait_time = between(1, 2)
@@ -77,9 +80,7 @@ class APIUser(HttpUser):
         self.client.post("/events", headers=headers, json=mock_event())
 
 
-   
-
 # tape personility - parcel
 # tape type - white
-# date format - mm/dd/yy 
+# date format - mm/dd/yy
 # APN in the db
